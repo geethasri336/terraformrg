f34028d41ab200fb2d24763238c90ca463e15ed8
geethasri336 <geethasri336@gmail.com>
2021-03-02 21:39:43 +0530

    rg files added

---
+++ main.tf
@@ -0,0 +1,10 @@
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg10"
+  location = "West Europe"
+  tags={
+
+env=prod
+
+}
+}
---
+++ provider.tf
@@ -0,0 +1,9 @@
+provider "azurerm" {
+  features {}
+version = "=2.46.0"
+
+  subscription_id = "cb93420a-671a-4117-b90e-39ff612dc0eb"
+  client_id       = "bda4ed72-dced-4cb0-a0b7-7ca8fe3fb223"
+  client_secret   = "hBYl8.2R0B8o1F_GwL~_U.kQ3Kmxs13uj4"
+  tenant_id       = "653b073d-e2c3-498d-a5d9-4f7aacefaf3b"
+}
